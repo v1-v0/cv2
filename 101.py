6e9190ca7839945a7b81e4bd2c06ea470931f45c
v1v0 <hochiyau@hotmail.com>
2026-07-04 13:40:09 +0800

    Initial commit

---
+++ 101.py
@@ -0,0 +1,12 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+from zipfile import ZipFile
+from urllib.request import urlretrieve
+
+from IPython.display import Image
+
+#matplotlib inline
+
